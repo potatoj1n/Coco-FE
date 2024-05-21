c9925e0c4b8506bce9756b2a80de7b3c95419505
rbfl1260 <seven0126@naver.com>
2024-05-21 12:13:35 +0900

    닉네임 수정

--- src/pages/Mypage/MyPage.tsx
+++ src/pages/Mypage/MyPage.tsx
@@ -12,9 +12,16 @@ const MyPage = () => {
   const navigate = useNavigate();
   const [isReadOnly, setIsReadOnly] = useState(true);
   const nickNameRef = useRef<HTMLInputElement>(null);
-  const { email, nickname } = useAuthStore();
-  const [nickName, setNickname] = useState(nickname); //서버에서 받아오기
-  const [password, setPassword] = useState('password'); //서버에서 받아오기
+  // const { email, nickname } = useAuthStore();
+  const { nickname, email, setAuthInfo, memberId } = useAuthStore(state => ({
+    nickname: state.nickname,
+    email: state.email,
+    setAuthInfo: state.setAuthInfo,
+    memberId: state.memberId,
+  }));
+
+  const [nickName, setNickname] = useState(nickname); //저장소에서 받아오기
+  const [password, setPassword] = useState('1234'); //서버에서 받아오기
   const [isModalOpen, setIsModalOpen] = useState(false);
   const { themeColor } = useTheTheme();
 
@@ -37,32 +44,35 @@ const MyPage = () => {
   const onCloseModal = () => {
     setIsModalOpen(false); // 모달을 닫아줍니다
   };
-  const handlePasswordChange = (newPassword: string) => {
-    setPassword(newPassword);
-  };
+  // const handlePasswordChange = (newPassword: string) => {
+  //   setPassword(newPassword);
+  // };
   //저장버튼 눌렀을 때 현재 상태 서버로 전송
   const handleSaveClick = async () => {
     try {
       // 서버에 데이터를 POST 요청으로 보내는 예시
-      const response = await address.post('api/members/myPage', {
+      const response = await address.put('api/members/profile', {
         nickname: nickName,
-        newPassword: password,
+        password: password,
+        memberId: memberId,
       });
-      if (response.status === 200) {
-        alert('정보가 성공적으로 업데이트되었습니다.');
-      } else {
-        alert('업데이트에 실패했습니다.');
-      }
+      console.log(response.data.nickname);
+      // setNickname(response.data.nickname);
+      setAuthInfo(email, response.data.nickname, memberId);
+      // if (response.status === 200) {
+      //   alert('정보가 성공적으로 업데이트되었습니다.');
+      // } else {
+      //   alert('업데이트에 실패했습니다.');
+      // }ㅌ
     } catch (error) {
-      const err = error as Error;
-      if (err.message === ('Token expired' || 'Unauthorized or token expired')) {
-        // 요청 인터셉터에서 발생한 토큰 만료 에러
-        alert('Session expired, please login again.');
-        navigate('/login'); // 사용자를 로그인 페이지로 이동
-      } else {
-        console.error('업데이트 중 오류 발생', err);
-        alert('업데이트 중 서버 오류가 발생했습니다.');
-      }
+      // const err = error as Error;
+      // if (err.message === ('Token expired' || 'Unauthorized or token expired')) {
+      //   // 요청 인터셉터에서 발생한 토큰 만료 에러
+      //   alert('Session expired, please login again.');
+      //   navigate('/login'); // 사용자를 로그인 페이지로 이동
+      // } else {
+      console.error('업데이트 중 오류 발생');
+      // }
     }
   };
   return (
@@ -145,12 +155,7 @@ const MyPage = () => {
                 onChange={OnChange}
               />
               <Edit onClick={() => handleEditClick()} />
-              <Confirmpassword
-                isOpen={isModalOpen}
-                onClose={onCloseModal}
-                theme={themeColor}
-                onPasswordChange={handlePasswordChange}
-              />
+              <Confirmpassword isOpen={isModalOpen} onClose={onCloseModal} theme={themeColor} />
             </div>
           </div>
           <Save onClick={handleSaveClick}>저장</Save>
